c170a13a5fe154caf57573b6496855b7bb01e3f8
Ngungila <168288981+Ngungila@users.noreply.github.com>
2026-01-23 13:20:22 +0300

    Add View facade import for HTML email templates

--- app/Services/EmailNotificationService.php
+++ app/Services/EmailNotificationService.php
@@ -6,6 +6,7 @@ use App\Models\Setting;
 use App\Models\User;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\View;
 
