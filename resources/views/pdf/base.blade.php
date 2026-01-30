0e3d8d277b3a53477b5a791be5fbd17768e6e09e
Ngungila <168288981+Ngungila@users.noreply.github.com>
2026-01-30 10:42:44 +0300

    Fix base PDF layout to support @section('title') directive

--- resources/views/pdf/base.blade.php
+++ resources/views/pdf/base.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>{{ $title ?? 'Document' }} - FeedTan CMG</title>
+    <title>@yield('title', $title ?? 'Document') - FeedTan CMG</title>
     <style>
         @page {
             margin: 10mm 12mm;
